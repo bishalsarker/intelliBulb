535cea9b2a00db3e24cd8775f81ed9232bff0fc7
Bishal Sarker <mr.bishalsarker@outlook.com>
2019-09-05 16:37:59 +0600

    files added

---
+++ serialPortArduino/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace serialPortArduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::serialPortArduino.Properties.Resources.graphics_microphone_889791;
+            this.pictureBox1.Location = new System.Drawing.Point(121, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 108);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.label1.Location = new System.Drawing.Point(12, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sleeping...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.label2.Location = new System.Drawing.Point(12, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(266, 35);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Click anywhere to make me awake";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "intelliBulb";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+
+    }
+}
+
---
+++ serialPortArduino/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Speech.Recognition;
+
+namespace serialPortArduino
+{
+    public partial class Form1 : Form
+    {
+        SerialPort port;
+        int cmdState = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            if (cmdState == 0)
+            {
+                cmdState = 1;
+                label1.Text = "Listening...";
+                label2.Visible = false;
+                string cmd = "";
+                while (cmd != "sleep")
+                {
+                    SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine();
+                    Grammar g = new DictationGrammar();
+                    recognizer.LoadGrammar(g);
+                    recognizer.SetInputToDefaultAudioDevice();
+                    string rec = "";
+                    try
+                    {
+                        RecognitionResult result = recognizer.Recognize();
+                        rec = result.Text.ToLower();
+                    }
+                    catch (NullReferenceException c)
+                    {
+                        rec = "000";
+                    }
+
+                    if (rec.IndexOf("on") != -1)
+                    {
+                        sendCmd("on");
+                        label1.Text = "Turning lights on...";
+                        cmd = "000";
+                    }
+                    else if (rec.IndexOf("off") != -1)
+                    {
+                        sendCmd("off");
+                        label1.Text = "Turning lights off...";
+                        cmd = "000";
+                    }
+                    else if (rec.IndexOf("close") != -1)
+                    {
+                        label1.Text = "Sleeping...";
+                        label2.Visible = true;
+                        cmd = "sleep";
+                        cmdState = 0;
+                    }
+                    else
+                    {
+                        label1.Text = "What?...";
+                    }
+                }
+            }
+        }
+
+        private void sendCmd(string c)
+        {
+            if (c == "on")
+            {
+                port = new SerialPort();
+                port.PortName = "COM3";
+                port.BaudRate = 9600;
+                port.Open();
+                port.Write("1");
+                port.Close();
+            }
+
+            else if (c == "off")
+            {
+                port = new SerialPort();
+                port.PortName = "COM3";
+                port.BaudRate = 9600;
+                port.Open();
+                port.Write("0");
+                port.Close();
+            }
+            else
+            {
+                c = "000";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
